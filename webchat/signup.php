1dba4ae015326a4eb1823bd3b64748191bc017d9
Kapilsaini-actor <76694270+Kapilsaini-actor@users.noreply.github.com>
2020-12-28 19:59:17 +0530

    New Project Upload

---
+++ webchat/signup.php
@@ -0,0 +1,17 @@
+<?php
+include 'dbh.php';
+$uname=$_POST['uname'];
+$email=$_POST['email'];
+$pass=$_POST['password'];
+
+$sql="insert into signup(username,email,password) values('$uname','$email','$pass')";
+$result=$conn->query($sql);
+
+header("location:index.php");
+
+
+
+
+
+
+?>
